bbb2003e54ba345334fc4a21e74ef37ef970b669
IAN MOSHI <ian@IANs-MacBook-Air.local>
2026-02-27 22:07:59 +0300

    third commit

--- core/settings.py
+++ core/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'qissadb',
-        'USER': 'postgres',
-        'PASSWORD': 'ian ',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- stories/urls.py
+++ stories/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import story_detail, chapter_reader, payment_page
+from .views import chapter_reader, payment_page, story_detail
 
 urlpatterns = [
     path("story/<slug:slug>/", story_detail, name="story_detail"),
     path("chapter/<int:id>/", chapter_reader, name="chapter_reader"),
 
-    path("payment/<int:chapter_id>/", payment_page, name="payment_page"),
+    path("payment/<int:story_id>/", payment_page, name="payment_page"),
 
 ]
